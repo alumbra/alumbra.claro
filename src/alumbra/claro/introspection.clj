d48ad2e219d65db150f31200f3d20072d40c90ff
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-13 21:28:42 +0100

    reduce number of different resolvables in introspection.
    Makes it less likely to hit the batch/depth limit.

--- src/alumbra/claro/introspection.clj
+++ src/alumbra/claro/introspection.clj
@@ -9,7 +9,7 @@
 ;; the single types beforehand and just to a simple lookup within the 'Type'
 ;; resolvable.
 
-(declare ->Directive ->EnumValues ->Fields ->NestedType ->Type)
+(declare ->Directive ->EnumValues ->Fields ->Type as-nested-type)
 
 (defmacro ^:private defrecord-
   [type & body]
@@ -57,54 +57,50 @@
 
 ;; ### Nested Types (Non-Null and List)
 
-(defrecord- NestedType [nested-type-description]
-  data/Resolvable
-  (resolve! [_ _]
-    (let [{:keys [type-description non-null? type-name]}
-          nested-type-description]
-      (cond non-null?
-            (make-type-map
-              {:kind   :NON_NULL
-               :ofType (->NestedType
-                         (dissoc nested-type-description :non-null?))})
+(defn- as-nested-type
+  [nested-type-description]
+  (let [{:keys [type-description non-null? type-name]}
+        nested-type-description]
+    (cond non-null?
+          (make-type-map
+            {:kind   :NON_NULL
+             :ofType (as-nested-type
+                       (dissoc nested-type-description :non-null?))})
 
-            type-description
-            (make-type-map
-              {:kind   :LIST
-               :ofType (->NestedType type-description)})
+          type-description
+          (make-type-map
+            {:kind   :LIST
+             :ofType (as-nested-type type-description)})
 
-            :else
-            (->Type type-name)))))
+          :else
+          (->Type type-name))))
 
 ;; ### Arguments
 
-(defrecord- Argument [argument]
-  data/Resolvable
-  (resolve! [_ _]
-    (let [{:keys [argument-name type-description]} argument]
-      {:name         argument-name
-       :description  nil
-       :type         (->NestedType type-description)
-       ;; FIXME: default value for arguments
-       :defaultValue nil})))
+(defn- as-argument
+  [argument]
+  (let [{:keys [argument-name type-description]} argument]
+    {:name         argument-name
+     :description  nil
+     :type         (as-nested-type type-description)
+     ;; FIXME: default value for arguments
+     :defaultValue nil}))
 
-(defrecord- Arguments [arguments]
-  data/Resolvable
-  (resolve! [_ _]
-    (mapv ->Argument arguments)))
+(defn- as-arguments
+  [arguments]
+  (mapv as-argument arguments))
 
 ;; ### Fields
 
-(defrecord- Field [field]
-  data/Resolvable
-  (resolve! [_ _]
-    (let[{:keys [field-name type-description arguments]} field]
-      {:name              field-name
-       :description       nil
-       :args              (->Arguments (vals arguments))
-       :type              (->NestedType type-description)
-       :isDeprecated      false
-       :deprecationReason nil})))
+(defn- as-field
+  [field]
+  (let[{:keys [field-name type-description arguments]} field]
+    {:name              field-name
+     :description       nil
+     :args              (as-arguments (vals arguments))
+     :type              (as-nested-type type-description)
+     :isDeprecated      false
+     :deprecationReason nil}))
 
 (defrecord- Fields [fields includeDeprecated]
   data/Resolvable
@@ -114,67 +110,62 @@
         (keep
           (fn [{:keys [field-name] :as field}]
             (when (not= field-name "__typename")
-              (->Field field)))
+              (as-field field)))
           fields)))))
 
 ;; ### Object Types
 
-(defrecord- ObjectType [name]
-  data/Resolvable
-  (resolve! [_ {:keys [alumbra/analyzed-schema]}]
-    (let [{:keys [fields implements]}
-          (get-in analyzed-schema [:types name])]
-      (make-type-map
-        {:name       name
-         :kind       :OBJECT
-         :fields     (->Fields (vals fields) nil)
-         :interfaces (mapv ->Type implements)}))))
+(defn- as-object-type
+  [{:keys [alumbra/analyzed-schema]} name]
+  (let [{:keys [fields implements]}
+        (get-in analyzed-schema [:types name])]
+    (make-type-map
+      {:name       name
+       :kind       :OBJECT
+       :fields     (->Fields (vals fields) nil)
+       :interfaces (mapv ->Type implements)})))
 
 ;; ### Interface Types
 
-(defrecord- InterfaceType [name]
-  data/Resolvable
-  (resolve! [_ {:keys [alumbra/analyzed-schema]}]
-    (let [{:keys [fields implemented-by]}
-          (get-in analyzed-schema [:interfaces name])]
-      (make-type-map
-        {:name          name
-         :kind          :INTERFACE
-         :fields        (->Fields (vals fields) nil)
-         :possibleTypes (mapv ->Type implemented-by)}))))
+(defn as-interface-type
+  [{:keys [alumbra/analyzed-schema]} name]
+  (let [{:keys [fields implemented-by]}
+        (get-in analyzed-schema [:interfaces name])]
+    (make-type-map
+      {:name          name
+       :kind          :INTERFACE
+       :fields        (->Fields (vals fields) nil)
+       :possibleTypes (mapv ->Type implemented-by)})))
 
 ;; ### Union Types
 
-(defrecord- UnionType [name]
-  data/Resolvable
-  (resolve! [_ {:keys [alumbra/analyzed-schema]}]
-    (let [{:keys [union-types]}
-          (get-in analyzed-schema [:unions name])]
-      (make-type-map
-        {:name          name
-         :kind          :UNION
-         :possibleTypes (mapv ->Type union-types)}))))
+(defn- as-union-type
+  [{:keys [alumbra/analyzed-schema]} name]
+  (let [{:keys [union-types]}
+        (get-in analyzed-schema [:unions name])]
+    (make-type-map
+      {:name          name
+       :kind          :UNION
+       :possibleTypes (mapv ->Type union-types)})))
 
 ;; ### Scalar Types
 
-(defrecord- ScalarType [name]
-  data/Resolvable
-  (resolve! [_ _]
-    (make-type-map
-      {:name name
-       :kind :SCALAR})))
+(defn- as-scalar-type
+  [_ name]
+  (make-type-map
+    {:name name
+     :kind :SCALAR}))
 
 ;; ### Input Types
 
-(defrecord- InputType [name]
-  data/Resolvable
-  (resolve! [_ {:keys [alumbra/analyzed-schema]}]
-    (let [{:keys [fields]}
-          (get-in analyzed-schema [:input-types name])]
-      (make-type-map
-        {:name          name
-         :kind          :INPUT_OBJECT
-         :fields        (->Fields (vals fields) nil)}))))
+(defn- as-input-type
+  [{:keys [alumbra/analyzed-schema]} name]
+  (let [{:keys [fields]}
+        (get-in analyzed-schema [:input-types name])]
+    (make-type-map
+      {:name          name
+       :kind          :INPUT_OBJECT
+       :fields        (->Fields (vals fields) nil)})))
 
 ;; ### Enum Types
 
@@ -190,31 +181,31 @@
            :deprecationReason nil})
         enum-values))))
 
-(defrecord- EnumType [name]
-  data/Resolvable
-  (resolve! [_ {:keys [alumbra/analyzed-schema]}]
-    (let [enum-values (get-in analyzed-schema [:enums name])]
-      (make-type-map
-        {:name          name
-         :kind          :ENUM
-         :enumValues    (->EnumValues enum-values nil)}))))
+(defn- as-enum-type
+  [{:keys [alumbra/analyzed-schema]} name]
+  (let [enum-values (get-in analyzed-schema [:enums name])]
+    (make-type-map
+      {:name          name
+       :kind          :ENUM
+       :enumValues    (->EnumValues enum-values nil)})))
 
 ;; ### Dispatch Resolvable
 
 (defn dispatch-type-record
-  [{:keys [type->kind]} type-name]
-  (case (type->kind type-name)
-    :type       (->ObjectType type-name)
-    :interface  (->InterfaceType type-name)
-    :union      (->UnionType type-name)
-    :scalar     (->ScalarType type-name)
-    :enum       (->EnumType type-name)
-    :input-type (->InputType type-name)))
+  [{:keys [alumbra/analyzed-schema] :as env} type-name]
+  (let [{:keys [type->kind]} analyzed-schema]
+    (case (type->kind type-name)
+      :type       (as-object-type env type-name)
+      :interface  (as-interface-type env type-name)
+      :union      (as-union-type env type-name)
+      :scalar     (as-scalar-type env type-name)
+      :enum       (as-enum-type env type-name)
+      :input-type (as-input-type env type-name))))
 
 (defrecord Type [name]
   data/Resolvable
-  (resolve! [_ {:keys [alumbra/analyzed-schema]}]
-    (dispatch-type-record analyzed-schema name)))
+  (resolve! [_ env]
+    (dispatch-type-record env name)))
 
 ;; ## Directives
 
@@ -230,7 +221,7 @@
                             (string/replace "-" "_")
                             (string/upper-case)))
                       directive-locations)
-       :args        (->Arguments (vals arguments))})))
+       :args        (as-arguments (vals arguments))})))
 
 ;; ## Middleware
 
