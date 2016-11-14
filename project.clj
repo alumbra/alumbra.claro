b887d3384833d0bdf1fe2a155639f28b3065b528
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-11-14 11:30:24 +0100

    make Clojure dependency 'provided'.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://opensource.org/licenses/MIT"
             :year 2016
             :key "mit"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [alumbra/spec "0.1.0-SNAPSHOT"]
                  [claro "0.2.4-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[alumbra/validator "0.1.0-SNAPSHOT"]
