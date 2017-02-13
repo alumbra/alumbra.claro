d7765b1b6bb51624f8581830824959dc1c97a025
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-02-13 14:26:41 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -6,11 +6,11 @@
             :year 2016
             :key "mit"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
-                 [alumbra/spec "0.1.5" :scope "provided"]
+                 [alumbra/spec "0.1.6" :scope "provided"]
                  [camel-snake-kebab "0.4.0"]
-                 [claro "0.2.6"]]
+                 [claro "0.2.7"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [alumbra/parser "0.1.4"]
-                                  [alumbra/analyzer "0.1.3"]
+                                  [alumbra/analyzer "0.1.5"]
                                   [alumbra/generators "0.2.2"]]}}
   :pedantic? :abort)
