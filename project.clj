c0dc9ea7a125edbb465aafca9903d0579b3bcf89
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-02-22 15:47:58 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [alumbra/spec "0.1.6" :scope "provided"]
                  [camel-snake-kebab "0.4.0"]
-                 [claro "0.2.7"]]
+                 [claro "0.2.9"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
-                                  [alumbra/parser "0.1.4"]
-                                  [alumbra/analyzer "0.1.6"]
+                                  [alumbra/parser "0.1.5"]
+                                  [alumbra/analyzer "0.1.7"]
                                   [alumbra/generators "0.2.2"]]}}
   :pedantic? :abort)
