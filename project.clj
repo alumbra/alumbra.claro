b846c35bd090a01beef401c51f5298a7d2dcdbcb
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-04-19 13:57:22 +0200

    upgrade 'claro' dependency.

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14" :scope "provided"]
                  [alumbra/spec "0.1.6" :scope "provided"]
                  [camel-snake-kebab "0.4.0"]
-                 [claro "0.2.9"]]
+                 [claro "0.2.13"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [alumbra/parser "0.1.5"]
                                   [alumbra/analyzer "0.1.7"]
