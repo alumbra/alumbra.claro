6ff12c2540e6651e5734599a1ee26eb523b46d19
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-05-17 17:12:26 +0200

    Version 0.1.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alumbra/claro "0.1.14-SNAPSHOT"
+(defproject alumbra/claro "0.1.14"
   :description "An alumbra GraphQL executor on top of Claro."
   :url "https://github.com/alumbra/alumbra.claro"
   :license {:name "MIT License"
